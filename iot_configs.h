ae4000c41051d3acab6594b4d4396fc7ab2e953a
Ahmed Fathy <ahmedfathy11876@gmail.com>
2025-02-08 00:37:55 +0200

    Sending Data from Esp8266 to Azure iot hub

---
+++ iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "summit"
+#define IOT_CONFIG_WIFI_PASSWORD "paradiselost12"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "kakashihub.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "kakashidev"
+#define IOT_CONFIG_DEVICE_KEY "9XgzqgFmDHRUgeK4K8JuUO/tQKv+CBfttEZ0UZFJoIk="
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 2000
