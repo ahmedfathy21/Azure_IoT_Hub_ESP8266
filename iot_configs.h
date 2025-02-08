a1ed7305d725ad12dfe361eb99b031c125488feb
Ahmed Fathy <ahmedfathy11876@gmail.com>
2025-02-08 06:40:14 +0200

    Update Config file

--- iot_configs.h
+++ iot_configs.h
@@ -2,13 +2,13 @@
 // SPDX-License-Identifier: MIT
 
 // Wifi
-#define IOT_CONFIG_WIFI_SSID "summit"
-#define IOT_CONFIG_WIFI_PASSWORD "paradiselost12"
+#define IOT_CONFIG_WIFI_SSID "WIFI_SSID"
+#define IOT_CONFIG_WIFI_PASSWORD "WIFI_PASSWORD"
 
 // Azure IoT
-#define IOT_CONFIG_IOTHUB_FQDN "kakashihub.azure-devices.net"
-#define IOT_CONFIG_DEVICE_ID "kakashidev"
-#define IOT_CONFIG_DEVICE_KEY "9XgzqgFmDHRUgeK4K8JuUO/tQKv+CBfttEZ0UZFJoIk="
+#define IOT_CONFIG_IOTHUB_FQDN " IOT_HUB_NAME.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "DEVICE_ID"
+#define IOT_CONFIG_DEVICE_KEY "DEVICE_KEY"
 
 // Publish 1 message every 2 seconds
 #define TELEMETRY_FREQUENCY_MILLISECS 2000
